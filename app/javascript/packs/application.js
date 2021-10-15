56f3f2322396cce7a84f26d733dc7039cf41bdf0
Danila Berazhny <dannmmaster@gmail.com>
2021-10-15 15:36:47 +0300

    Customize home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../stylesheets/aplication.scss"
 
 Rails.start()
 Turbolinks.start()
