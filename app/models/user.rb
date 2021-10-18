66f9dec0988fa36702b20f7cb671e024c52d3e5d
Danila Berazhny <dannmmaster@gmail.com>
2021-10-18 16:04:53 +0300

    Add one-to-many association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_many :articles
 end
