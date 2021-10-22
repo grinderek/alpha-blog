ea852cb34f9656803bfca8b521b9a9524946b2e8
Danila Berazhny <dannmmaster@gmail.com>
2021-10-22 23:31:35 +0300

    Add funtional test for create and add create action with template

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,33 @@
+class CategoriesController < ApplicationController
+  before_action :category_params, only: [:create]
+
+
+  def new
+    @category = Category.new
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
