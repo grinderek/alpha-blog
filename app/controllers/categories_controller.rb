d47d53b73b6d7cdb1fd7515bb0299656b728c3a6
Danila Berazhny <dannmmaster@gmail.com>
2021-10-23 00:41:36 +0300

    Add new integration test for reject invalid category submission

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,6 +18,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
+      flash[:notice] = "Category was successfully created"
       redirect_to @category
     else
       render 'new'
