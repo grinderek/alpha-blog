7d6eff4a3b0ad0539b048590ed0089be01845643
Danila Berazhny <dannmmaster@gmail.com>
2021-10-23 01:14:27 +0300

    Implement admin user requirement

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @admin_user = User.create(username: "john", email: "john@email.com", password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test "get new category form and create cattegory" do
     get "/categories/new"
     assert_response :success
