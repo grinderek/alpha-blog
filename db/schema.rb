4231c166609f69c615017dafae291fe6c49adea3
Danila Berazhny <dannmmaster@gmail.com>
2021-10-23 01:43:59 +0300

    Add associations from UI

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,19 @@
 
 ActiveRecord::Schema.define(version: 2021_10_22_222019) do
 
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-  end
-
-  create_table "articles_categories", force: :cascade do |t|
-    t.integer "article_id"
-    t.integer "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", force: :cascade do |t|
